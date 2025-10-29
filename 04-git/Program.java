d7e8261d5344676dd6e701d9a4696dca2d9cd0c3
Inga Grabska <inga.grabska@gmail.com>
2025-10-29 12:45:11 +0100

    fixed reference to variable

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -4,8 +4,8 @@ public class Program {
   public static void main(String[] args) {
     Program programik = new Program();
     
-    System.out.println(program.x);
-    System.out.println(program.x);
+    System.out.println(programik.x);
+    System.out.println(programik.x);
   }
 
 }
