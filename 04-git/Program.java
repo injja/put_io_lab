d2639e69f31b0f4aa3803a626cfc97a94672adfd
Inga Grabska <inga.grabska@gmail.com>
2025-10-29 12:45:11 +0100

    fixed reference to variable

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,11 +1,11 @@
-public class Program {
+public class Program { //klasa ktora wyświetla liczbę
   int x = 52;
 
   public static void main(String[] args) {
     Program programik = new Program();
     
-    System.out.println(program.x);
-    System.out.println(program.x);
+    System.out.println(programik.x);
+    System.out.println(programik.x);
   }
 
 }
