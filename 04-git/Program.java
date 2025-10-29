c0bb22f152d5495f434cf46462cb63a2e95016df
Inga Grabska <inga.grabska@gmail.com>
2025-10-29 12:44:45 +0100

    changed name of variable

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,7 +2,7 @@ public class Program {
   int x = 52;
 
   public static void main(String[] args) {
-    Program program = new Program();
+    Program programik = new Program();
     
     System.out.println(program.x);
     System.out.println(program.x);
